c43e3ff8cd97539b59dd7e314fbe7a9df93e468f
alexandr <k4t4kun@gmail.com>
2022-01-04 18:24:44 +0900

    6.9. Жадный алгоритм. Сдача в кофе машине [#34741 #246716] Изменения.

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -7,9 +7,10 @@ public class Machine {
         int[] coins = {10, 5, 2, 1};
         int[] rsl = new int[100];
         int size = 0;
+        money -= price;
 
         for (int coin : coins) {
-            while (money - coin >= price) {
+            while (money >= coin) {
                 money -= coin;
                 rsl[size] = coin;
                 size++;
